dba6dac695423ea2f1bfd68c0bfb238629033273
Carmen Cuevas <ccguerrero.ps@gmail.com>
2024-11-10 01:14:36 -0300

    se agrega menú de opciones, clase alumno, funciones y array

--- proyecto1.js
+++ proyecto1.js
@@ -1,5 +1,87 @@
-alert("Ingrese el nombre y las 6 notas finales de las asignaturas de su alumno")
-let nombreAlumno = prompt("Ingrese nombre del alumno")
+// alert("Ingrese el nombre y las 6 notas finales de las asignaturas de su alumno")
+
+class Alumno {
+    constructor(nombre, notaLeng, notaMate, notaCs, notaIng, notaHist) {
+        this.nombre = nombre
+        this.notaLeng = notaLeng
+        this.notaMate = notaMate
+        this.notaCs = notaCs
+        this.notaIng = notaIng
+        this.notaHist = notaHist
+        this.promedioFinalAlumno = this.promedioFinal(notaLeng, notaMate, notaCs, notaIng, notaHist)
+    }
+    promedioFinal(notaLeng, notaMate, notaCs, notaIng, notaHist){
+        return (notaLeng + notaMate + notaCs + notaIng + notaHist)/5
+    }
+}
+
+function dataAlumnos(arregloAlumnos) {
+    const nombreAlumno = prompt("Ingrese nombre del alumno")
+    const notaLeng = parseFloat(prompt("Ingrese nota final de Lenguaje"))
+    const notaMate = parseFloat(prompt("Ingrese nota final de Matemática"))
+    const notaCs = parseFloat(prompt("Ingrese nota final de Ciencias"))
+    const notaIng = parseFloat(prompt("Ingrese nota final de Inglés"))
+    const notaHist = parseFloat(prompt("Ingrese nota final de Historia"))
+    
+    let nuevoALumno = new Alumno(nombreAlumno, notaLeng, notaMate, notaCs, notaIng, notaHist)
+    
+    arregloAlumnos.push(nuevoALumno)
+
+    let opcion = Number(prompt("Quiere hacer un nuevo ingreso?\n 1.- Si\n 2.- No"))
+
+    if (opcion==1) {
+        dataAlumnos(arregloAlumnos)
+    }
+    
+    inicial(arregloAlumnos)
+}
+
+function getPromedioFinal(arregloAlumnos) {
+    if (arregloAlumnos.length != 0) {
+        const nombreAlumno = prompt("Ingrese nombre del alumno")
+        const resultado = arregloAlumnos.find((alumno) => alumno.nombre === nombreAlumno)
+        alert(`El promedio de ${resultado.nombre} es ${resultado.promedioFinalAlumno}`)
+    }else{
+        alert("Error. No hay notas ingresadas de los alumnos")
+    }
+    inicial(arregloAlumnos)
+}
+
+function inicial(arregloAlumnos) {
+    let opcionMenu = Number(prompt("MENÚ\n\n Seleccione una opción:\n 1.- Nuevo ingreso\n 2.- Promedio final del alumno\n 3.- Promedio final del curso por asignatura\n 4.- Lista alumnos aprobados \n 5.- Salir")) 
+
+    
+    switch (opcionMenu) {
+        case 1:
+            dataAlumnos(arregloAlumnos)
+            break;
+        case 2:
+            getPromedioFinal(arregloAlumnos)
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        default:
+            break;
+    }
+}
+
+let arregloAlumnos = []
+
+inicial(arregloAlumnos)
+
+
+
+
+
+
+
+
+
+
 
 function sumarNotas(){
     let suma = 0
@@ -8,7 +90,6 @@ function sumarNotas(){
         while (true) {
             nota = parseFloat(prompt("Ingrese nota de la asignatura N° " + index))
             if(!isNaN(nota)){
-                console.log(nota)
                 suma = suma + nota
                 break
             }else{
@@ -51,18 +132,18 @@ function mensajeFinal(isApproved, resultadoFinal, nivelAprobacion, nombreAlumno)
     }
 }
 
-let sumaTotal = sumarNotas()
+// let sumaTotal = sumarNotas()
 
-let resultadoFinal = promedioAlumno(sumaTotal)
+// let resultadoFinal = promedioAlumno(sumaTotal)
 
-let resultado = isApproved(resultadoFinal)
+// let resultado = isApproved(resultadoFinal)
 
-let aprobacion = nivelAprobacion(resultadoFinal)
+// let aprobacion = nivelAprobacion(resultadoFinal)
 
 
-mensajeFinal(resultado, resultadoFinal, aprobacion, nombreAlumno)
+// mensajeFinal(resultado, resultadoFinal, aprobacion, nombreAlumno)
 
-console.log(nombreAlumno)
-console.log(resultadoFinal)
-console.log(resultado)
-console.log(aprobacion)
+// console.log(nombreAlumno)
+// console.log(resultadoFinal)
+// console.log(resultado)
+// console.log(aprobacion)
